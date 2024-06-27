4950e9eaee1510819fe10382e95ce0288c4e8d98
SRIJITH11101 <dovetonsrijith29@gmail.com>
2024-06-27 15:53:40 +0530

    sub collection display

--- lib/Screens/display_screen2.dart
+++ lib/Screens/display_screen2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:task_02/Constants/constant.dart';
 import 'package:task_02/Service/Langapi.dart';
 
 class SecondDisplayScreen extends StatefulWidget {
@@ -11,8 +12,11 @@ class SecondDisplayScreen extends StatefulWidget {
 
 class _SecondDisplayScreenState extends State<SecondDisplayScreen> {
   LangApi lng = LangApi();
-  String? selectedFromLang = "Albanees";
-  String? selectedToLang = "Chinees (Kantonees)";
+  String? selectedFromLang;
+  String? selectedToLang;
+  int showList = 1;
+  bool fromLangFetched = false;
+  bool toLangFetched = false;
 
   @override
   void initState() {
@@ -22,37 +26,71 @@ class _SecondDisplayScreenState extends State<SecondDisplayScreen> {
   }
 
   void getLang() async {
-    await lng.getFromLang();
-    await lng.getToLang();
-    setState(() {});
+    await lng.getFromLang(context);
+    //await lng.getToLang(selectedFromLang!);
+    //selectedToLang = lng.toL[0];
+
+    setState(() {
+      fromLangFetched = true;
+      showList = 0;
+    });
+  }
+
+  void getToLang() async {
+    lng.toL = [];
+    selectedToLang = null;
+    print(lng.toL);
+    String selectedFromLangId = lng.fromLang[selectedFromLang]!;
+    await lng.getToLang(context, selectedFromLangId);
+    //selectedToLang = lng.toL[0];
+    setState(() {
+      toLangFetched = true;
+      showList = 0;
+    });
+  }
+
+  void getCategory() async {
+    lng.slug.clear();
+    lng.comSlug.clear();
+    String selectedFromLangId = lng.fromLang[selectedFromLang]!;
+    String selectedToLangId = lng.toLang[selectedToLang]!;
+    await lng.getSlug(context, selectedFromLangId, selectedToLangId);
+    setState(() {
+      if (lng.id.isEmpty) {
+        showList = 3;
+      } else {
+        showList = 2;
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Text(
-              'Language Display',
-              style: TextStyle(fontSize: 25),
-            ),
-            SizedBox(
-              height: 80,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  children: [
-                    Text('Select From Language'),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width / 2.2,
-                      child: Expanded(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(
+                'Language Display',
+                style: TextStyle(fontSize: 25),
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Text('Select From Language'),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width / 2.2,
                         child: DropdownButton(
+                          hint: Text('Select From Language'),
                           isExpanded: true,
                           // Initial Value
                           value: selectedFromLang,
@@ -69,51 +107,112 @@ class _SecondDisplayScreenState extends State<SecondDisplayScreen> {
                           onChanged: (String? newValue) {
                             setState(() {
                               //print(newValue);
+                              toLangFetched = false;
+                              showList = 1;
                               selectedFromLang = newValue;
+
+                              getToLang();
                               print(selectedFromLang);
                             });
                           },
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Text('Select To Language'),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width / 2.2,
-                      child: DropdownButton(
-                        isExpanded: true,
-                        // Initial Value
-                        value: selectedToLang,
-                        // Down Arrow Icon
-                        icon: const Icon(Icons.keyboard_arrow_down),
-                        // Array list of items
-                        items: lng.toL.map((String item) {
-                          return DropdownMenuItem(
-                            value: item,
-                            child: Text(item),
-                          );
-                        }).toList(),
-
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            //print(newValue);
-                            selectedToLang = newValue;
-                            print(selectedToLang);
-                          });
-                        },
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text('Select To Language'),
+                      SizedBox(
+                        height: 10,
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            )
-          ],
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width / 2.2,
+                        child: DropdownButton(
+                          hint: Text('Select To Language'),
+                          isExpanded: true,
+                          // Initial Value
+                          value: selectedToLang,
+                          // Down Arrow Icon
+                          icon: const Icon(Icons.keyboard_arrow_down),
+                          // Array list of items
+                          items: lng.toL.map((String item) {
+                            return DropdownMenuItem(
+                              value: item,
+                              child: Text(item),
+                            );
+                          }).toList(),
+
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              //print(newValue);
+                              selectedToLang = newValue;
+                              print(selectedToLang);
+                              lng.icon.clear();
+                              lng.name.clear();
+                              lng.id.clear();
+                              showList = 1;
+                              getCategory();
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              // TextButton(
+              //   onPressed: () {
+              //     setState(() {
+              //       lng.icon.clear();
+              //       lng.name.clear();
+              //       lng.id.clear();
+              //       showList = 1;
+              //       getCategory();
+              //     });
+              //   },
+              //   child: Text('Fetch the Sub-Category'),
+              // ),
+
+              showList == 1
+                  ? CircularProgressIndicator()
+                  : showList == 2
+                      ? ListView.builder(
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          itemCount: lng.finalCount,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              leading: SizedBox(
+                                  height: 500,
+                                  width: 50,
+                                  child: Image.network(
+                                      'https://angulars.babelonia.nl/${lng.icon[index]}')),
+                              title: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  //Text('Id : ${id[index]}'),
+                                  Align(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        lng.name[index],
+                                        style: bStyle,
+                                      )),
+                                ],
+                              ),
+                            );
+                          },
+                        )
+                      : showList == 3
+                          ? Text(
+                              'No data Available',
+                              style: TextStyle(fontSize: 15),
+                            )
+                          : SizedBox(),
+            ],
+          ),
         ),
       ),
     );
--- lib/Service/Langapi.dart
+++ lib/Service/Langapi.dart
@@ -1,51 +1,110 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:task_02/Service/Apicalls.dart';
+import 'package:task_02/Widgets/snackbar.dart';
 
 class LangApi {
+  ApiCalls apicls = ApiCalls();
   Map<String, String> fromLang = {};
   Map<String, String> toLang = {};
   List<String> fromL = [];
   List<String> toL = [];
+  List<String> slug = [];
+  List<String> comSlug = [];
+  List<String> id = [];
+  List<String> icon = [];
+  List<String> name = [];
+  int finalCount = 0;
 
-  Future getFromLang() async {
+  Future getFromLang(BuildContext context) async {
     var uri = 'https://api.babelonia.nl/site/home/source-category-list';
     var url = Uri.parse(uri);
-    http.Response response = await http.get(url);
-    if (response.statusCode == 200) {
-      var data = response.body;
-      var jsonData = jsonDecode(data);
-      List<dynamic> details = jsonData['response'];
-      int count = details.length;
-      for (var i = 0; i < count; i++) {
-        fromL.add(jsonData['response'][i]['name']);
-        fromLang[jsonData['response'][i]['name']] =
-            jsonData['response'][i]['_id'];
+    try {
+      http.Response response = await http.get(url);
+      if (response.statusCode == 200) {
+        var data = response.body;
+        var jsonData = jsonDecode(data);
+        List<dynamic> details = jsonData['response'];
+        int count = details.length;
+        for (var i = 0; i < count; i++) {
+          fromL.add(jsonData['response'][i]['name']);
+          fromLang[jsonData['response'][i]['name']] =
+              jsonData['response'][i]['_id'];
+        }
+        print(fromL);
+        print(fromLang);
       }
-      print(fromL);
-      print(fromLang);
+      return response.body;
+    } catch (e) {
+      showsSnackbar(context: context, content: e.toString());
     }
-    return response.body;
   }
 
-  Future getToLang() async {
+  Future getToLang(BuildContext context, String fromLang) async {
     var uri =
-        'https://api.babelonia.nl/site/home/target-category-list?id=5fe3539dfcd01b7ed6c35dfb';
+        'https://api.babelonia.nl/site/home/target-category-list?id=${fromLang}';
     var url = Uri.parse(uri);
-    http.Response response = await http.get(url);
-    if (response.statusCode == 200) {
-      var data = response.body;
-      var jsonData = jsonDecode(data);
-      List<dynamic> details = jsonData['response'];
-      int count = details.length;
-      for (var i = 0; i < count; i++) {
-        toL.add(jsonData['response'][i]['name']);
-        toLang[jsonData['response'][i]['name']] =
-            jsonData['response'][i]['_id'];
+
+    try {
+      http.Response response = await http.get(url);
+      if (response.statusCode == 200) {
+        var data = response.body;
+        var jsonData = jsonDecode(data);
+        List<dynamic> details = jsonData['response'];
+        int count = details.length;
+        for (var i = 0; i < count; i++) {
+          toL.add(jsonData['response'][i]['name']);
+          toLang[jsonData['response'][i]['name']] =
+              jsonData['response'][i]['_id'];
+        }
+        print(toL);
+        print(toLang);
       }
-      print(toL);
-      print(toLang);
+      return response.body;
+    } catch (e) {
+      showsSnackbar(context: context, content: e.toString());
     }
-    return response.body;
+  }
+
+  Future getSlug(BuildContext context, String fromLang, String toLang) async {
+    var uri =
+        'https://api.babelonia.nl/site/home/sub-category-list?langone=${fromLang}&langtwo=${toLang}';
+    var url = Uri.parse(uri);
+    try {
+      http.Response response = await http.get(url);
+      if (response.statusCode == 200) {
+        var data = response.body;
+        var jsonData = jsonDecode(data);
+        var comJsonData = await apicls.getdetails();
+        List<dynamic> comDetails = comJsonData['response'];
+        List<dynamic> details = jsonData['response'];
+        int count = details.length;
+        for (var i = 0; i < count; i++) {
+          slug.add(jsonData['response'][i]['slug']);
+        }
+        print(slug);
+        int comCount = comDetails.length;
+        for (var i = 0; i < comCount; i++) {
+          comSlug.add(comJsonData['response'][i]['slug']);
+        }
+        print(comSlug);
+        for (var i = 0; i < comCount; i++) {
+          for (var j = 0; j < count; j++) {
+            if (slug[j].contains(comSlug[i])) {
+              id.add(comJsonData['response'][i]['_id']);
+              icon.add(comJsonData['response'][i]['icon']);
+              name.add(comJsonData['response'][i]['name']);
+            }
+          }
+        }
+        finalCount = id.length;
+        print(id);
+      }
+      return response.body;
+    } catch (e) {
+      showsSnackbar(context: context, content: e.toString());
+    }
   }
 }
